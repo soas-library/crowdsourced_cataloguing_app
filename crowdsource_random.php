5c4a8487e2e245e95822f23119e5ba746b622e5a
SimonXIX <sb174@soas.ac.uk>
2019-03-18 16:51:36 +0000

    Further progress on crowdsourcing app

---
+++ crowdsource_random.php
@@ -0,0 +1,133 @@
+<?php
+# @name: crowdsource_random.php
+# @version: 0.1
+# @creation_date: 2019-03-08
+# @license: The MIT License <https://opensource.org/licenses/MIT>
+# @author: Simon Bowie <sb174@soas.ac.uk>
+# @purpose: A prototype of a web application to crowdsource cataloguing for SOAS' bibliographic records
+?>
+<html>
+<body>
+
+<?php
+	$solrurl = 'http://james.lis.soas.ac.uk:8983/solr/bib/select?fl=bibIdentifier&fq=DocType:item&fq=Language_search:Bengali&indent=on&q=*&rows=5000&wt=xml';
+	
+	# Perform Curl request on the Solr API
+	$ch = curl_init();
+	$queryParams = $bib_id;
+	curl_setopt($ch, CURLOPT_URL, $solrurl);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_HEADER, FALSE);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+	$response = curl_exec($ch);
+	curl_close($ch);
+	
+ 	# Turn the API response into useful XML
+	$xml = new SimpleXMLElement($response); 
+
+	$random = rand(0,4061);
+	
+	$bib_id = ltrim($xml->result->doc[$random]->arr->str, "wbm-");
+	$baseurl = 'https://james.lis.soas.ac.uk:8443/oledocstore/documentrest/';
+	$retrieve_bib = '/bib/doc?bibId=';
+	
+	# Perform Curl request on the OLE API
+	$ch = curl_init();
+	$queryParams = $bib_id;
+	curl_setopt($ch, CURLOPT_URL, $baseurl . $retrieve_bib . $queryParams);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_HEADER, FALSE);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+	$response = curl_exec($ch);
+	curl_close($ch);
+	
+	# Turn the API response into useful XML
+	$xml = new SimpleXMLElement($response); 
+	
+	$content = $xml->content;
+	$content = simplexml_load_string($content);
+?>
+	<form action="crowdsource_submit.php" method="POST">
+		<fieldset>
+			<legend><strong>Fix Me</strong></legend><br/>
+			<strong>Title: </strong>
+<?php
+
+				foreach ($content->record->datafield as $datafield) {
+					if ((string) $datafield['tag'] == '245') {
+						foreach ($datafield->subfield as $subfield) {
+							echo (string) $subfield . " ";
+						}
+					}
+				}
+
+?>
+			<br/>
+			<input type="text" name="title"><br/><br/>
+			
+			<strong>Alternative title: </strong>
+<?php
+
+				foreach ($content->record->datafield as $datafield) {
+					if ((string) $datafield['tag'] == '246') {
+						foreach ($datafield->subfield as $subfield) {
+							echo (string) $subfield . " ";
+						}
+					}
+				}
+
+?>
+			<br/>
+			<input type="text" name="alt_title"><br/><br/>
+			
+			<strong>Main author: </strong>
+<?php
+
+				foreach ($content->record->datafield as $datafield) {
+					if ((string) $datafield['tag'] == '100') {
+						foreach ($datafield->subfield as $subfield) {
+							echo (string) $subfield . " ";
+						}
+					}
+				}
+
+?>
+			<br/>
+			<input type="text" name="author"><br/><br/>
+			
+			<strong>Publication details: </strong>
+<?php	
+
+				foreach ($content->record->datafield as $datafield) {
+					if ((string) $datafield['tag'] == '260' || (string) $datafield['tag'] == '264') {
+						foreach ($datafield->subfield as $subfield) {
+							echo (string) $subfield . " ";
+						}
+					}
+				}
+
+?>
+			<br/>
+			<input type="text" name="publication"><br/><br/>
+			
+			<strong>Comment: </strong>
+			<br/>
+			<input type="text" name="comment"><br/><br/>
+			
+			<input type="hidden" value="
+			<?php 
+				foreach ($content->record->controlfield as $controlfield) {
+					if ((string) $controlfield['tag'] == '001') {
+						echo (string) $controlfield;
+					}
+				}
+			?>
+			" name="id" />
+			
+		</fieldset>
+		<div>
+			<input type="submit" name="submit" value="Submit">
+		</div>
+	</form>
+</body>
+</html>
---
+++ crowdsource_search.php
@@ -0,0 +1,125 @@
+<?php
+# @name: crowdsource_search.php
+# @version: 0.1
+# @creation_date: 2019-03-08
+# @license: The MIT License <https://opensource.org/licenses/MIT>
+# @author: Simon Bowie <sb174@soas.ac.uk>
+# @purpose: A prototype of a web application to crowdsource cataloguing for SOAS' bibliographic records
+?>
+<html>
+<body>
+
+<?php
+	$solrurl = 'http://james.lis.soas.ac.uk:8983/solr/bib/select?fl=bibIdentifier&fq=DocType:item&fq=Language_search:Bengali&indent=on&q=Title_search:' . $_POST["search"] . '%20OR%20Author_search:' . $_POST["search"] . '%20OR%20Publisher_search:' . $_POST["search"] . '%20OR%20PublicationDate_search:' . $_POST["search"] . '%20OR%20PublicationPlace_search:' . $_POST["search"] . '&rows=5000&wt=xml';
+	#$solrurl = 'http://james.lis.soas.ac.uk:8983/solr/bib/select?fl=bibIdentifier,Title_display,Author_display,Publisher_display,PublicationPlace_display,PublicationDate_display&fq=DocType:item&fq=Language_search:Bengali&indent=on&q=Title_search:' . $_POST["search"] . '%20OR%20Author_search:' . $_POST["search"] . '%20OR%20Publisher_search:' . $_POST["search"] . '%20OR%20PublicationDate_search:' . $_POST["search"] . '%20OR%20PublicationPlace_search:' . $_POST["search"] . '&rows=5000&wt=xml';
+	
+	# Perform Curl request on the Solr API
+	$ch = curl_init();
+	$queryParams = $bib_id;
+	curl_setopt($ch, CURLOPT_URL, $solrurl);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_HEADER, FALSE);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+	$response = curl_exec($ch);
+	curl_close($ch);
+	
+ 	# Turn the API response into useful XML
+	$xml = new SimpleXMLElement($response); 
+	
+	if ($xml->result->attributes()->numFound == '0'){
+		echo 'No results found';
+	}
+	else {
+		// foreach ($xml->result->doc as $result) {
+			// foreach ($result->arr as $result_array) {
+				// print_r($result_array->str);
+			// }
+		// }
+		#print_r($xml->result->doc);
+		foreach ($xml->result->doc as $result){
+			foreach ($result->arr->str as $id){
+				$bib_id = ltrim($id, "wbm-");
+				$baseurl = 'https://james.lis.soas.ac.uk:8443/oledocstore/documentrest/';
+				$retrieve_bib = '/bib/doc?bibId=';
+	
+				# Perform Curl request on the OLE API
+				$ch = curl_init();
+				$queryParams = $bib_id;
+				curl_setopt($ch, CURLOPT_URL, $baseurl . $retrieve_bib . $queryParams);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+				curl_setopt($ch, CURLOPT_HEADER, FALSE);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+				$response = curl_exec($ch);
+				curl_close($ch);
+	
+				# Turn the API response into useful XML
+				$xml = new SimpleXMLElement($response); 
+	
+				$content = $xml->content;
+				$content = simplexml_load_string($content);
+			
+				echo "<h3>Record</h3>";
+			
+				foreach ($content->record->datafield as $datafield) {
+					if ((string) $datafield['tag'] == '245') {
+						foreach ($datafield->subfield as $subfield) {
+							echo (string) $subfield . " ";
+						}
+					}
+				}
+				
+				echo "<br/>";
+				
+				foreach ($content->record->datafield as $datafield) {
+					if ((string) $datafield['tag'] == '246') {
+						foreach ($datafield->subfield as $subfield) {
+							echo (string) $subfield . " ";
+						}
+					}
+				}
+				
+				echo "<br/>";
+				
+				foreach ($content->record->datafield as $datafield) {
+					if ((string) $datafield['tag'] == '100') {
+						foreach ($datafield->subfield as $subfield) {
+							echo (string) $subfield . " ";
+						}
+					}
+				}
+				
+				echo "<br/>";
+				
+				foreach ($content->record->datafield as $datafield) {
+					if ((string) $datafield['tag'] == '260' || (string) $datafield['tag'] == '264') {
+						foreach ($datafield->subfield as $subfield) {
+							echo (string) $subfield . " ";
+						}
+					}
+				}
+				
+				echo "<br/>";
+?>
+				
+			<form action="crowdsource_edit.php" method="POST">
+				<input type="hidden" value="
+			<?php 
+				foreach ($content->record->controlfield as $controlfield) {
+					if ((string) $controlfield['tag'] == '001') {
+						echo (string) $controlfield;
+					}
+				}
+			?>
+				" name="id" />
+			
+				<div>
+					<input type="submit" name="submit" value="Edit">
+				</div>
+<?php
+			}
+		}
+	}
+?>
+
+</body>
+</html>
