71f6da964fe05c89bc595c8b765fcb002caae751
SimonXIX <sb174@soas.ac.uk>
2019-05-17 13:59:25 +0100

    Enhanced submission to Google Sheets API

--- crowdsource_submit.php
+++ crowdsource_submit.php
@@ -6,6 +6,27 @@
 # @author: Simon Bowie <sb174@soas.ac.uk>
 # @purpose: A prototype of a web application to crowdsource cataloguing for SOAS' bibliographic records
 ?>
+<?php
+
+	// clean up all inputted data
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+	
+	// define variables and set to empty values
+	$id = $title = $alt_title = $author = $publication = $comment = "";
+	
+	$id = $_POST["id"];
+	$title = test_input($_POST["title"]);
+	$alt_title = test_input($_POST["alt_title"]);
+	$author = test_input($_POST["author"]);
+	$publication = test_input($_POST["publication"]);
+	$comment = test_input($_POST["comment"]);
+	
+?>
 
 <?php
 require __DIR__ . '/vendor/autoload.php';
@@ -34,88 +55,30 @@ $client->setAuthConfig(__DIR__ . '/crowdsource-ecca04407a4e.json');
  */
 $sheets = new \Google_Service_Sheets($client);
 
-/*
- * To read data from a sheet we need the spreadsheet ID and the range of data we want to retrieve.
- * Range is defined using A1 notation, see https://developers.google.com/sheets/api/guides/concepts#a1_notation
- */
-$data = [];
-
-// The first row contains the column titles, so lets start pulling data from row 2
-$currentRow = 2;
-
-// The range of A2:H will get columns A through H and all rows starting from row 2
-#$spreadsheetId = getenv('SPREADSHEET_ID');
 $spreadsheetId = '1dM-YM2-qxIy_CJ95SknO02X330j91PP5d3lFlYiCd_Q';
-$range = 'A2:H';
-$rows = $sheets->spreadsheets_values->get($spreadsheetId, $range, ['majorDimension' => 'ROWS']);
-if (isset($rows['values'])) {
-    foreach ($rows['values'] as $row) {
-        /*
-         * If first column is empty, consider it an empty row and skip (this is just for example)
-         */
-        if (empty($row[0])) {
-            break;
-        }
+$range = 'Sheet1';
+$valueInputOption = 'USER_ENTERED';
 
-        $data[] = [
-            'col-a' => $row[0],
-            'col-b' => $row[1],
-            'col-c' => $row[2],
-            'col-d' => $row[3],
-            'col-e' => $row[4],
-            'col-f' => $row[5],
-            'col-g' => $row[6],
-            'col-h' => $row[7],
-        ];
+$values = [
+		[
+			$id,
+			$title,
+			$alt_title,
+			$author,
+			$publication,
+			$comment,
+			date('c')
+		]
+];
 
-        /*
-         * Now for each row we've seen, lets update the I column with the current date
-         */
-        $updateRange = 'B'.$currentRow;
-        $updateBody = new \Google_Service_Sheets_ValueRange([
-            'range' => $updateRange,
-            'majorDimension' => 'ROWS',
-            'values' => ['values' => date('c')],
-        ]);
-        $sheets->spreadsheets_values->update(
-            $spreadsheetId,
-            $updateRange,
-            $updateBody,
-            ['valueInputOption' => 'USER_ENTERED']
-        );
+$body = new \Google_Service_Sheets_ValueRange([
+    'values' => $values
+]);
+$params = [
+    'valueInputOption' => $valueInputOption
+];
+$result = $sheets->spreadsheets_values->append($spreadsheetId, $range, $body, $params);
 
-        $currentRow++;
-    }
-}
+printf("%d cells appended.", $result->getUpdates()->getUpdatedCells());
 
-print_r($data);
-/* Output:
-Array
-(
-    [0] => Array
-        (
-            [col-a] => 123
-            [col-b] => test
-            [col-c] => user
-            [col-d] => test user
-            [col-e] => usertest
-            [col-f] => email@domain.com
-            [col-g] => yes
-            [col-h] => no
-        )
-
-    [1] => Array
-        (
-            [col-a] => 1234
-            [col-b] => another
-            [col-c] => user
-            [col-d] =>
-            [col-e] => another
-            [col-f] => another@eom.com
-            [col-g] => no
-            [col-h] => yes
-        )
-
-)
- */
 ?>
