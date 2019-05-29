73a1535e9b76c8a2867fb0f1ff52ca3f88429aa7
SimonXIX <sb174@soas.ac.uk>
2019-05-29 11:24:55 +0100

    Edits to submit page

--- crowdsource_submit.php
+++ crowdsource_submit.php
@@ -6,6 +6,36 @@
 # @author: Simon Bowie <sb174@soas.ac.uk>
 # @purpose: A prototype of a web application to crowdsource cataloguing for SOAS' bibliographic records
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>SOAS Library Bengali cataloguing</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/soas-favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/soas.css">
+<!--===============================================================================================-->
+</head>
+<body>
+
 <?php
 
 	// clean up all inputted data
@@ -81,6 +111,35 @@ $params = [
 ];
 $result = $sheets->spreadsheets_values->append($spreadsheetId, $range, $body, $params);
 
-printf("%d cells appended.", $result->getUpdates()->getUpdatedCells());
-
 ?>
+
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-form p-l-55 p-r-55 p-t-150 p-b-50">
+					<span class="login100-form-title">
+						Help us learn Bengali
+					</span>
+
+					<div class="content100">
+						<div class="wrap-content100">
+							Thank you for contributing to SOAS Library's Bengali cataloguing enhancement project.
+						</div>
+						
+						<div class="flex-col-c p-t-50 p-b-40">
+							<span class="txt1 p-b-9">
+								We have received
+							
+<?php
+						$result = $sheets->spreadsheets_values->get($spreadsheetId, $range);
+						$numRows = ($result->getValues() != null ? count($result->getValues()) : 0) - 1;
+						printf("%d", $numRows);
+?>
+								 contributions so far.
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
