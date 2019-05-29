9cad420e7ae6f7d56b6793cdf8f8333a4d80b707
SimonXIX <sb174@soas.ac.uk>
2019-05-29 12:20:42 +0100

    Added ending page

---
+++ crowdsource_thanks.php
@@ -0,0 +1,100 @@
+<?php
+# @name: crowdsource_thanks.php
+# @version: 0.1
+# @creation_date: 2019-05-29
+# @license: The MIT License <https://opensource.org/licenses/MIT>
+# @author: Simon Bowie <sb174@soas.ac.uk>
+# @purpose: A prototype of a web application to crowdsource cataloguing for SOAS' bibliographic records
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>SOAS Library Bengali cataloguing</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/soas-favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/soas.css">
+<!--===============================================================================================-->
+</head>
+<body>
+
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+/*
+ * We need to get a Google_Client object first to handle auth and api calls, etc.
+ */
+$client = new \Google_Client();
+$client->setApplicationName('crowdsource');
+$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+$client->setAccessType('offline');
+
+/*
+ * The JSON auth file can be provided to the Google Client in two ways, one is as a string which is assumed to be the
+ * path to the json file. This is a nice way to keep the creds out of the environment.
+ *
+ * The second option is as an array. For this example I'll pull the JSON from an environment variable, decode it, and
+ * pass along.
+ */
+#$jsonAuth = getenv('JSON_AUTH');
+#$client->setAuthConfig(json_decode($jsonAuth, true));
+$client->setAuthConfig(__DIR__ . '/crowdsource-ecca04407a4e.json');
+
+/*
+ * With the Google_Client we can get a Google_Service_Sheets service object to interact with sheets
+ */
+$sheets = new \Google_Service_Sheets($client);
+
+$spreadsheetId = '1dM-YM2-qxIy_CJ95SknO02X330j91PP5d3lFlYiCd_Q';
+$range = 'Sheet1';
+
+?>
+
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-form p-l-55 p-r-55 p-t-150 p-b-50">
+					<span class="login100-form-title">
+						Help us learn Bengali
+					</span>
+
+					<div class="content100">
+						<div class="wrap-content100">
+							Thank you for contributing to SOAS Library's Bengali cataloguing enhancement project.
+						</div>
+						
+						<div class="flex-col-c p-t-50 p-b-40">
+							<span class="txt1 p-b-9">
+								We have received
+							
+<?php
+						$result = $sheets->spreadsheets_values->get($spreadsheetId, $range);
+						$numRows = ($result->getValues() != null ? count($result->getValues()) : 0) - 1;
+						printf("%d", $numRows);
+?>
+								 contributions so far.
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
