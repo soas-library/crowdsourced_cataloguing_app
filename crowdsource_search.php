766de4fc0a2840235e3e0623ef24a3ebc16648b0
SimonXIX <sb174@soas.ac.uk>
2019-06-19 16:24:53 +0100

    Closed divs on search page

--- crowdsource_search.php
+++ crowdsource_search.php
@@ -198,13 +198,17 @@
 		}
 	}
 ?>
-		<span class="flex-col-c p-b-20">
-			<a href="crowdsource_about.php">About the project</a>
-		</span>
-		<span class="flex-col-c p-b-40">
-			<a href="mailto:libenquiry@soas.ac.uk">Send feedback</a>
-		</span>	
+					<span class="flex-col-c p-b-20">
+						<a href="crowdsource_about.php">About the project</a>
+					</span>
+					<span class="flex-col-c p-b-40">
+						<a href="mailto:libenquiry@soas.ac.uk">Send feedback</a>
+					</span>	
+				</div>
+			</div>
+		</div>
 	</div>
+</div>
 			
 <!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
