2e6dae08350c4fe0e97a47c4ef61be64196bb1af
SimonXIX <sb174@soas.ac.uk>
2019-06-25 11:37:13 +0100

    Added 'home' link on more pages

--- crowdsource_thanks.php
+++ crowdsource_thanks.php
@@ -105,8 +105,11 @@ $range = 'submissions';
 				<span class="flex-col-c p-b-20">
 					<a href="crowdsource_about.php">About the project</a>
 				</span>
-				<span class="flex-col-c p-b-40">
+				<span class="flex-col-c p-b-20">
 					<a href="mailto:libenquiry@soas.ac.uk">Send feedback</a>
+				</span>
+				<span class="flex-col-c p-b-40">
+					<a href="index.php">Home</a>
 				</span>
 			</div>
 		</div>
